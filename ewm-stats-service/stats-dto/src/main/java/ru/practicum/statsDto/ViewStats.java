b3068a694549b870c67c7ac90db388500753fdb0
Elena Semseeva <e.semseeva@yandex.ru>
2023-11-17 11:15:04 +0300

    fix: remove @Getter and @Setter in ViewStats.java

--- ewm-stats-service/stats-dto/src/main/java/ru/practicum/statsDto/ViewStats.java
+++ ewm-stats-service/stats-dto/src/main/java/ru/practicum/statsDto/ViewStats.java
@@ -1,11 +1,8 @@
 package ru.practicum.statsDto;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
+
 @AllArgsConstructor
 public class ViewStats {
     public String app;
